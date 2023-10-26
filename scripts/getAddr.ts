84896793b92bc6b313508e56448b57bb15cdd44e
AlexNi245 <alex.plutta@googlemail.com>
2023-10-26 14:22:40 +0200

    cleanup script section

--- scripts/getAddr.ts
+++ scripts/getAddr.ts
@@ -1,12 +1,13 @@
-import { dnsEncode } from "ethers/lib/utils";
 import { ethers } from "hardhat";
 
 export const getAddr = async () => {
-    const resolver = await ethers.provider.getResolver(process.env.ENS_NAME);
+    const provider = new ethers.providers.StaticJsonRpcProvider(process.env.L1_PROVIDER_URL);
+    const resolver = await provider.getResolver(process.env.ENS_NAME);
+    console.log("getting address record");
 
-    console.log("start getAddr");
     const addr = await resolver.getAddress();
     console.log(addr);
+
 };
 
 getAddr();
--- scripts/getText.ts
+++ scripts/getText.ts
@@ -1,12 +1,11 @@
 import hre, { ethers } from "hardhat";
 
 export const getText = async () => {
-    const [signer] = await hre.ethers.getSigners();
-    const resolver = await ethers.provider.getResolver(process.env.ENS_NAME);
 
-    console.log("start getTEXT");
-    const text = await resolver.getText("my-record");
-
+    const provider = new ethers.providers.StaticJsonRpcProvider(process.env.L1_PROVIDER_URL);
+    const resolver = await provider.getResolver(process.env.ENS_NAME);
+    console.log("getting text record");
+    const text = await resolver.getText("description");
     console.log(text);
 };
 
