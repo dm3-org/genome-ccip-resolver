9296a5c93f44b440f24fdd741001aa9967f80e19
AlexNi245 <alex.plutta@googlemail.com>
2024-01-04 09:04:59 +0100

    use mainnet SpaceId Identifier

--- server/utils/spaceIDNode.ts
+++ server/utils/spaceIDNode.ts
@@ -1,9 +1,10 @@
 import { Address, namehash, toHex } from "viem"
 
-const GNO_IDENTIFIER = BigInt('274997945614032132263423446017095573970170942858695765128406315342190546')
+//Space ID Node Identifier for Gnosis Mainnet. Changes for every network.
+const GNO_MAINNET_IDENTIFIER = BigInt('2702484275810670337286593638197304166435784191035983069259851825108946')
 
 export function getSpaceIdNode(inputName: string): Address {
-    const fullNameNode = `${inputName}.[${toHex(GNO_IDENTIFIER, { size: 32 }).slice(2)}]`
+    const fullNameNode = `${inputName}.[${toHex(GNO_MAINNET_IDENTIFIER, { size: 32 }).slice(2)}]`
     return namehash(fullNameNode)
 
 }
